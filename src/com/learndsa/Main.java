785f3f639cd1a62f853672faed4a9450cd77ce75
sagarsubedi <sagarsubediofficial@gmail.com>
2021-09-03 21:51:46 -0400

    just the first commit to check

---
+++ src/com/learndsa/Main.java
@@ -0,0 +1,11 @@
+package com.learndsa;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("just a test");
+
+    } // end of main
+
+
+} // end of class
